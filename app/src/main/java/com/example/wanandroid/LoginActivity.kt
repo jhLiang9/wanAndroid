ab2eb2571b92384a0cc54fd1a98bcf1313f87e5f
梁俊豪 <1342348969@qq.com>
2021-06-08 02:59:17 +0800

    Update

--- app/src/main/java/com/example/wanandroid/LoginActivity.kt
+++ app/src/main/java/com/example/wanandroid/LoginActivity.kt
@@ -1,16 +1,20 @@
 package com.example.wanandroid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.WindowManager
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);//隐藏状态栏？
         setContentView(R.layout.activity_login)
+        setSupportActionBar(logintoolbar)
 
     }
 
@@ -20,10 +24,13 @@ class LoginActivity : AppCompatActivity() {
     }
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
-            R.id.more -> Toast.makeText(this, "You clicked More",
+            R.id.more -> Toast.makeText(this, "You clicked HELP",
                 Toast.LENGTH_SHORT).show()
-            R.id.register -> Toast.makeText(this, "You clicked register",
-                Toast.LENGTH_SHORT).show()
+            R.id.register ->{
+                Toast.makeText(this, "You clicked register", Toast.LENGTH_SHORT).show()
+                val intent=Intent(this,RegisterActivity::class.java)
+                startActivity(intent)
+            }
         }
         return true
     }
--- app/src/main/java/com/example/wanandroid/MainActivity.kt
+++ app/src/main/java/com/example/wanandroid/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.wanandroid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);//
         setContentView(R.layout.activity_main)
+        login.setOnClickListener {
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
