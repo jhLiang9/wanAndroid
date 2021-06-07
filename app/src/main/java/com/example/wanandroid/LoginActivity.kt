61b5b5a459759475ec8215c4ce91d3f0d87fe9bd
梁俊豪 <1342348969@qq.com>
2021-06-08 02:19:40 +0800

    UI update

--- app/src/main/java/com/example/wanandroid/LoginActivity.kt
+++ app/src/main/java/com/example/wanandroid/LoginActivity.kt
@@ -2,10 +2,12 @@ package com.example.wanandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
     }
 }
