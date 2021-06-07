96f5aa02ad1dbf54f8298ed09d5ace2adff41fda
梁俊豪 <1342348969@qq.com>
2021-06-08 02:30:46 +0800

    UI update

--- app/src/main/java/com/example/wanandroid/LoginActivity.kt
+++ app/src/main/java/com/example/wanandroid/LoginActivity.kt
@@ -2,6 +2,9 @@ package com.example.wanandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
@@ -9,5 +12,19 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.logintoolbar,menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.more -> Toast.makeText(this, "You clicked More",
+                Toast.LENGTH_SHORT).show()
+            R.id.register -> Toast.makeText(this, "You clicked register",
+                Toast.LENGTH_SHORT).show()
+        }
+        return true
     }
 }
