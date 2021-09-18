798aa8622d65606a6bfc416df38aea9d00e8157f
jhliang9 <jhliang9@gmail.com>
2021-09-18 21:34:58 +0800

    added function in BaseActivity

--- app/src/main/java/com/example/wanandroid/activity/baseactivity/BaseActivity.kt
+++ app/src/main/java/com/example/wanandroid/activity/baseactivity/BaseActivity.kt
@@ -1,8 +1,12 @@
 package com.example.wanandroid.activity.baseactivity
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.example.wanandroid.R
+import com.example.wanandroid.fragment.MyCollectionFragment
 import com.example.wanandroid.service.AppService
 import com.example.wanandroid.service.ServiceCreator
 import com.example.wanandroid.viewmodel.SearchViewModel
@@ -12,4 +16,14 @@ open class BaseActivity : AppCompatActivity(){
 
     fun <T : ViewModel> getViewModel(modelClass: Class<T>): T  = ViewModelProvider(this).get(modelClass)
     protected val appService = ServiceCreator.create(AppService::class.java)
+    private val fragmentTransaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+    protected fun replaceFragment(id:Int,fragment: Fragment){
+        fragmentTransaction.replace(id,fragment).commit()
+    }
+    protected fun addFragment(id:Int,fragment: Fragment){
+        fragmentTransaction.add(id,fragment).commit()
+    }
+    protected fun addFragment(id:Int,fragment: Fragment,tag:String){
+        fragmentTransaction.add(id,fragment,tag)
+    }
 }
